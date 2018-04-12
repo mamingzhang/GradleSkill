3744f8e09ef4f14a24f16f13cb87e67d96eb9a1f
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-04-12 12:15:13 +0800

    module增加flavor配置，手动增加，后期尝试自动

--- app/src/main/java/com/horsege/mskill/MainActivity.java
+++ app/src/main/java/com/horsege/mskill/MainActivity.java
@@ -3,6 +3,8 @@ package com.horsege.mskill;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.horsege.library.Library;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -10,7 +12,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Product product = new Product();
-        product.toastProduct(this);
+//        Product product = new Product();
+//        product.toastProduct(this);
+
+        Library library = new Library();
+        library.toastLibrary(this);
     }
 }
---
+++ library/src/product_two/java/com/horsege/library/Library.java
@@ -0,0 +1,11 @@
+package com.horsege.library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Library {
+    public void toastLibrary(Context context) {
+        Toast.makeText(context, "Library Two", Toast.LENGTH_SHORT).show();
+
+    }
+}
